eaeb4c8f99e17e5f6eb581c7689750ca5ee43cf8
zeeshana <zeeshanahmed1b@gmail.com>
2019-10-27 01:43:04 +0300

    d

--- server-local.js
+++ server-local.js
@@ -1,5 +1,7 @@
 'use strict';
 
 const app = require('./express/server');
+const  cors = require('cors')
 
+app.use(cors());
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
