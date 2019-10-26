34355f44969e29493447e88a5a5b0c7910d61e0a
zeeshana <zeeshanahmed1b@gmail.com>
2019-10-27 00:44:33 +0300

    d

--- express/server.js
+++ express/server.js
@@ -4,6 +4,18 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const result = require('dotenv').config();
+
+const oauth = require('oauth');
+const _twitterConsumerKey = process.env.TWITTER_CONSUMER_KEY;
+const _twitterConsumerSecret = process.env.TWITTER_CONSUMER_SECRET;
+const twitterCallbackUrl = process.env.TWITTER_CALLBACK_URL;
+const consumer = new oauth.OAuth("https://twitter.com/oauth/request_token", "https://twitter.com/oauth/access_token",_twitterConsumerKey, _twitterConsumerSecret, "1.0A", twitterCallbackUrl, "HMAC-SHA1");
+
+
+if (result.error) {
+  throw result.error
+}
 
 const router = express.Router();
 router.get('/', (req, res) => {
@@ -13,10 +25,28 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/connect', (req, res) => {
+	console.log('connect insite');
+  consumer.getOAuthRequestToken(function (error, oauthToken,   oauthTokenSecret, results) {
+    if (error) {
+      res.send(error, 500);
+    } else {
+      req.session.oauthRequestToken = oauthToken;
+      req.session.oauthRequestTokenSecret = oauthTokenSecret;
+      const redirect = { 
+redirectUrl: `https://twitter.com/oauth/authorize?oauth_token=${req.session.oauthRequestToken}`
+    }
+      res.send(redirect);
+    }
+  });
+});
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
 
 module.exports = app;
 module.exports.handler = serverless(app);
